a0f5075b90b3f558cc2fb37137b06bfa982cb8be
Brayconn <29030756-Brayconn@users.noreply.github.com>
2020-07-31 01:28:31 -0700

    Copyright

--- LayeredPictureBox/Properties/AssemblyInfo.cs
+++ LayeredPictureBox/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LayeredPictureBox")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Brayconn 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
